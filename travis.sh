369a1e46f9bcad0c795b981ea02aa85b304f926f
sonia <sonia@gmail.com>
2022-01-19 13:55:59 +0000

    circle ci integration

--- travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash  
-set -ev
-
-HUBNAME=soniab/filedistributor;
-docker pull $HUBNAME || true
-docker build  --cache-from $HUBNAME  -t $HUBNAME .
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
-docker push $HUBNAME  
